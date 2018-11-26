75f2e4915e14e75ac3095a1ebf9d670faec7682f
Christopher Vatcher <christophervatcher@users.noreply.github.com>
2018-11-26 18:25:05 +0000

    Modified the RISC-V 64-bit arch/march parameter to be correct.

--- cworthy/targets/rv64.mk
+++ cworthy/targets/rv64.mk
@@ -4,6 +4,6 @@
 
 include $(CWORTHYDIR)/targets/riscv.mk
 
-TARGET_ARCH += -march=rv64imacfd -mabi=lp64d
-TARGET_MACH += -march=rv64imacfd -mabi=lp64d
+TARGET_ARCH += -march=rv64imafdc -mabi=lp64d
+TARGET_MACH += -march=rv64imafdc -mabi=lp64d
 
